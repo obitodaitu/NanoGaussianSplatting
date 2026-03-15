8e62cd496fd6d6e9c1d2703211a624b859a5555e
TimChen <37008451+TimChen1383@users.noreply.github.com>
2026-03-16 00:36:57 +0800

    clean up folders

--- Source/GSTile/GSTile.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "GSTile.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GSTile, "GSTile" );
